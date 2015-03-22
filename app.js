124eac19c1388981ada071c6d2fdabe19c682e78
gtg092x <mdrake@mediadrake.com>
2015-03-22 12:48:57 -0700

    added archive service

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var restify = require('restify');
 var controller = require('./controllers');
+var PATH = require('path');
+var ecstatic = require('ecstatic');
 
 function respond(req, res, next) {
-    res.send({hello: 'world'});
+    res.send({hello: 'build'});
     next();
 }
+
+
 
 var server = restify.createServer();
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 server.get('/', respond);
+
+server.get(/\/archive\/?.*/, restify.serveStatic({
+    directory: './static'
+}));
+
 server.get('/hook/:target', controller.hook);
 server.post('/hook/:target', controller.hook);
 
